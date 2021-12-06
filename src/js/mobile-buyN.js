3a56e0f96a76a124ecc6a2dd0f7e083d2850ef97
AnriSanches <sanchesanri@gmail.com>
2021-12-06 21:15:53 +0200

    version 4.0

---
+++ src/js/mobile-buyN.js
@@ -0,0 +1,29 @@
+(() => {
+    const mobileMenu = document.querySelector('.js-buy-container');
+    const openMenuBtn = document.querySelector('.js-open-buy');
+    const closeMenuBtn = document.querySelector('.js-close-buy');
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+  
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    // Закрываем мобильное меню на более широких экранах
+    // в случае изменения ориентации устройства.
+    window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
