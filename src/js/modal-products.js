b4e96c3bd13b79aecade7b6b5a2f6fdc165192ff
Oksana Semko <osemko@gmail.com>
2021-12-07 02:12:05 +0200

    add changes to modal-products

--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -1,6 +1,34 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[product-data-menu]');
+  const menuBtnRef = document.querySelector('[data-menu-button-1]');
+  const mobileMenuRef = document.querySelector('[product-data-menu-1]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button-2]');
+  const mobileMenuRef = document.querySelector('[product-data-menu-2]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button-3]');
+  const mobileMenuRef = document.querySelector('[product-data-menu-3]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
